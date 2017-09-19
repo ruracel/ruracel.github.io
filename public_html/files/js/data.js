76f4b52d900738b14c0dd0fd780321ecdc045c03
Andrei Ermicioi <andrei.ermicioi@gmail.com>
2017-09-19 14:41:11 +0200

    work

---
+++ public_html/files/js/data.js
@@ -0,0 +1,145 @@
+var data = {
+    slogan: 'Goodness also comes through music, because it is conditioned by love.',
+    menu: [{
+      title: 'Biography', anchor: 'biography'
+    },{
+        title: 'Repertoire', anchor: 'repertoire'
+    },{
+      title: 'Recordings', anchor: 'recordings'
+    },{
+      title: 'Gallery', anchor: 'gallery'
+    },{
+      title: 'Contacts', anchor: 'contacts'
+    }],
+    gallery: [[{
+        url: 'https://lh3.googleusercontent.com/Nkg17nkvA3cdzC0fjdBob66irqkvgp7B22_RYuAIr5nw28Png-qs4M00vGuyknqdG032Dtv3yummLWf-qgGnO5rdwIax2r8WNj6idf_u8NYH0sEaZiNXyFm7V158liaHxxyn2X3qIA=w634-h950-no',
+        cols: 4
+    },{
+        url: 'https://lh3.googleusercontent.com/1n2bcxnnkoJmcl5zN5AtRTsPI2GBsoEz6pN2laD9jTyla_agF16eq7ZQhP2FbZJdtN26xvpVdLUicMMfTgAGsPsaxiT95yOeKxaOB4Y9qu-oDEXqf7XnUPHQJWaXSQJFzwfiBZ6W6g=w634-h950-no',
+        cols: 4
+    },{
+        url: 'https://lh3.googleusercontent.com/_A2OY0Jgi4u-P-tTqt--eCIQEbWwGeZOWMR4idb_gYrDCjcBiaFqkhQE1d9tQJRKydeiPccCoPYC21KtzyCnxo6lhVHOvpqeNL53yXQd31R8dJZWPp1U28DPmbyaTVPBKI2kj4QINA=w634-h950-no',
+        cols: 4
+    }],[{
+        url: 'https://lh3.googleusercontent.com/dv4Zhl1Z8Ee1420QTlG-UxdGaFhaNosZV-iRtD87i-4BRCGo6vZTSF0q-QZR-b2JqH5T149Zn3WbvlAOnfJoEQnF9gLsWyTpyybSDLOzuRJhF1eNHycSfn5LtxFbw1AxNZJAhwW3XQ=w634-h950-no',
+        cols: 4
+    },{
+        url: 'https://lh3.googleusercontent.com/5ssyruKs4LiJh0NWirlI0PLZNldWRlNH2Pk8TbhKTCzv1PqLTvOQIOcY4PDny0RyNd31vj3SgfU45jigRHp6Z73r-KvclwGHRJClXnIHbbxapoLCWxL3IQo1EWVEFWBwgaRvQG1ntQ=w507-h760-no',
+        cols: 4
+    },{
+        url: 'https://lh3.googleusercontent.com/zqP-fe5kVBGu-zMPLWUX1pFhpbqgy1S1Dty5iUhp74jWIJ5lTZ8UfUEPcrbc9Zni35SUt6pDjMWUC0ud8R9oFGRPZoGZVl5yif6Y8jDDxr4_MIguBMuK90hNkCcGhSju4nFxRHNYfzxiODO2yVtjnHO49PE8d8e260s3xNy4w_jej2StVK7wd_Z8huw-PffAvWyZNuhWw4c04B8dPCFKBC789qQyNTGYY3laupENby85m_ubxH8unc6tfOrx0IhsKXNcIsfxe2vQIQRcLsAwkPVBjvRaKYWg710ek_vAkdWcXQTVFyDny4q_xLz086plA6mR0fthjgq03jxw3LuEZmYGQ2JbaIq76JTQDI3HImzagQt2TAQYeABL6zrDwg9_lvYPZtCHrqNnXkcf0AstYdQIR9NCwSA1bPg-M-Dm50f8Bugd9j2Uqjf7vo1kXzpjLkG0aoBn75L8350rmx_dx1VdaAbEXNB7p-yV6sKBn3zMxeSktM-qgrspuCm9pzgeSmdHZP4eYIu7Lj7KCS421vk0OOMIKYsGE9Oi77biJP2OgxYhGH-6eNFRUqxF2YwzuSovQxIjovbat97-JNoGr0d4BCYrhfBeyhs7EsYCpg=w634-h950-no',
+        cols: 4
+    }],[{
+        url: 'https://lh3.googleusercontent.com/0wLi2Z54NSpyGnmwpMVTzT1TGXcFaNlteI6dDZrQwEnqaEMk91JmCrUwWiRWI1fmcIIene7W_w4fN4KZY6ZrdiZra84KGqoxIFsgZJr5ekkjacVsE9YcGyPT23AFLW0zABG1Xz4qLQ=w1135-h950-no',
+        cols: 7
+    },{
+        url: 'https://lh3.googleusercontent.com/6NatAiAkmJIn76s33UI-rAzIYjKqN-7G1Kf2pNDSE-qPePkldvWj1PVE0Ff191HACRjdaA8XcAaepVs_lfS9_bJJMc-30v-gK2c92eeKHh3SFyZ3nxBKPIQLkDGs3LHj478RnA5FVjVbKSmxUe3KO39kqHNKg4x_RaW81tqROsn5Fzk2DsNl8R0_c_4eva9WG5a31F9HFPg5vSGENaDhSxamK_ySFZyiglY3iB5BRomIyDbKIsZxuUQ86kqZHkkv9eJnv5jZ5txIy0krtBj8DLl4gZRpkOF2qWBCO1u1dxRC-UZpqGpXnzDuOxRCwPCk3AVZbMK9sjvx1Sn-GJF7ag_LqXVbG_pv6x-30dVOe0oPKliPlH9HRmCCLJT2lnGLJ6eyefNFxVUJbzw97dEMLF-cfdCUAC1m7zFr-92gAoHvCqVLPFQI_X8ycZYPpnFpoEM64dj5fde7687igW6y0HYWOZ8HQCioSGAT40Dx4YLayDwsc4Qgw6EbSeqb6xJtK-KP8LM4swWScV1f0OMjnXE_ve914MZFGjb3xzG3PjLCp7d4aZOykEk_anfHP3htuZH2xzViV_Fgrqn23MDxTkYR9JttIDHEV12UJoSEAQ=w535-h768-no',
+        cols: 5
+    }],[{
+        url: 'https://lh3.googleusercontent.com/ysfFMUoMFgE78-uzr_DQoVJcmqD_k5R0gVHfu8ZN23bVTLlEZWxwBgCDTx88XPxEXPc60-_2ThUMMpAhnfuwH3f72bg9Z_0bE9fAYQU22-Aqsn6jq4y4BBKNCcNtcCs2UVvoTNnmntcYXvBBNKb-aTRIMotklTDEHpf7n_aqvgD0gKuvrzpomnZOxoIDD2Z9cv9qv_DMX7adqv3duzgyqv49sgoEvpmCH_dyzIlo-eyLVKN8oFvESdhxP-vSeHZqqZFEl07GuYZLmEBb-p2hrWtS3B3IvLzWI37BVD9maNgMbQfGqc9APLaBlCQ4jgUuWxt2Sr8UwvKT72Z_Cx02BpZ1wFOe85R5GsfrrrmsWDQda3CGjhEX0vR0ulHoH92HzZpP_iLmHchOzxiVbq-R5x1yME-G_D97HxFL_B22YW1ArQ27CCS1Avxmfqf5pYsY2JLPhKhdpFh8ZMwFaAPuvyRNguWVBDCW9e3KyKQ2fJl8o912iBdPM8xzmV6vysGDMiN0k3-A_HIFwGZenwRrQPJq2OGCPQqk9hdSyhr2WKtYaMuL811i5oymnVBd_tywZyjYcpUXIrKmIxO5lvbBwLsOgRLtMsNbuHYdCabPvg=w702-h950-no',
+        cols: 4
+    },{
+        url: 'https://lh3.googleusercontent.com/F0-ZDT6JB8thi0aXxh4LADSuYDezFOSDNk5z-d3kqUTyjM-ZNNaEQX8xQxJ8rkXDlIrPoQsTM8I6gjnnWTRfsuPVZ1cGiKpA2RAONOUZGdZGBDf7W8nBbXknBeCwIH4Sfu9giJeuL7KULMt0jLTR-P5n44tFeuSjQjDc3T4ODoWC1CJ6xfDzRmNFJTLWSD9-n5Ug3ZMIoNQKZ5ASVYkmaPruKt6mIVkWi6gm8Ex05yCpYg54uF1Ie8VKmqBttsYPjWJ92H2E6R4DySniBM1K1nnmbDilrOCidH7sJT_M7BcJj_IUvakZ_qKKl0oH3jIKH33NwUhU8gJnhSEwmOFhnuuVhdxCTem3Lv4yXCV68YTXVrLMZEp8Xj5P7MfLdjYBaAj21gKBdWVW5n92rELUpD2MiNaJRoiCc9b1cH21w9VJ5FntuvjPT-p0JlOwAcRjiM1mveScMzRAyaHQRP0miRyWvxb4e6TB17l5bG6JudGgJ5OCZLfUYhD13AyFQXsVHhNMHSQHfnh125E2gzKpyUKi5JDa5jMelgK4ROYiFYPNQkFauI2lxWu0gzmXxb9WcfcBs7wvPXb3oS__XOHOUzcjjWNjW9yVkmD5ZBrFCQ=w938-h477-no',
+        cols: 8
+    }],[{
+        url: 'https://lh3.googleusercontent.com/2hLCcUSJ99_61aUf0OvRqvVjYFUMpr-UzMZcGDOcR-WdgMS8Op5Hn1yUhUnuKAmvsFXBUqEa9ovGLP9HJDtNPmJIadYlK9HGxP1cXYSzEk8nQvsFj-LXIieVCQCKKu1oOCl2ThJIwm7IQ6-J5luIW_OFP6i0tLGots7nJl6u_VCYWB_cXWIKrlkqhHGJKiSWJLIq4eBdmdnSSRlW1ZZ_-lRBBf3AsemOw92ycprpbdw2cSdgX0jgsQYpVlES5vChO9FwED3LUmdG8LZVdb6H45I8u5CVCOa-FfG1kOoxAwonW8AhygyjoKVrZZeQt8Fw_7LdNGbaLLhX-tG7gsByhdPap9Uc-ReAyFZplSKd79A73pF_Zy1qscUrN4g84NQ9ys0D5bX9sh1Tvwbh4qSsWtilTKHSJlWIXS0nlv_0m7EJb1S2K9ofJ6skc2f-GK5QK588IESGC_AKpj-YBoz7PuX1b_dUMtdaIMoBCmLal8P0SZWkGpYccV-gIFTOJDzq2E0i3zcPf2Ee4jzndm1Wlh5G3NOIS2nhXQ0i3R4HVq8_sJufnQSzQ7YMifn2jpRdoyjnUUwPFTIIyJspgKnyLzpXBaSPygvCXT9ZrofDfA=w1095-h600-no',
+        cols: 6
+    },{
+        url: 'https://lh3.googleusercontent.com/fffhDIfjdfdVfHifoiIXStm72xQUgQcniHvMp4IY4erUEmDwxX2mvnkxgtUjHJJCFyswF2K3NEVfKvYtP20sQzzH-a36-H9FqHn033RcjUV71EHY6rXY6aPAvJNkBE7BxA_rU27gkYEC2_8f_s7Eco8deHsMLnLNjcqugsV7Za0UtyxGcw_8BeHr-6c-dUP09cG9uAdiD0rLPslhdM0SyMIfa7PfdICdrFqQ_ORmPN7rgmCIY5KQUdi3Mz8yFQhWDEjDX3oVps_ooDmQMoEWt94SGbfcyLxHzYMlGI1jNLSAYNuj_5b0BOmJoFf0Tgf-YOARb3fVCrGbWBM2dMMHIs7VdHFoaUNuT-ISaqSAJ76Cr39-FEI6eYLz0SP1_00F1RyQoQ-YlkIe5adibwLV7hmL9hLIdVIvFmjU87_7EFdj2-2s473E6VYHWqUXw79RA9w_4VToMVFaEL1apUX3A-dyntCS1Mvj25TuayfZn7i0-wwLn01V9RhocVa-B1Ug0yaERqjFgYW4jQhUShsFfJHDm3ma9WcaCKvabQLsOGzlTeb9-jFJ6z8pOUaWfTA9aZA-i9EuSP0X66N1WgH7JbOy96jkIsJOjZLPuuBXRA=w1124-h640-no',
+        cols: 6
+    }],[{
+        url: 'https://lh3.googleusercontent.com/op0C7Od_cQfWhO8c-fU7-E60bZ3a43nQv1Tw3jWnY_xyDFk80anOMThzJedQxSXniE7eDM_w35vNxBnQpRTa9zgqKNEZFLEoK5HmSpVWmtTCpYHGv4YeEZadw4uuMeC0sYH_7O1DmJdb3CX4iY83BUChG91kQV3617bKCkSjWqSven1kOQukf85K3eZSc3iEaMSiazLxMq26fYFOBmLYnWBuOKwjq0VCCKC8tadcEks5h7zXNUXLmHNH_RfI6a-Rt9Nwx4AaOUFc25XxyKs-Jc9QZJN_JusrJchmGPDj-Ar1uPs8d5uW6ByLlUFTG0Qp9tWB-L6TsAFrMtVbsZi-zeEJRVqJknTaM96xCBfQf6Ylzoo2WpeAGBUnaGcl5_VoLWKNFlJK4W5dxj7tmzVZkMm0azaoRFZuTEXlQGrjKIOIxx1VM5qmUwA-C-NdTB4A0nvf-bbNfs8reSy3bHqshUVGu0Sn3vqPFQW0KDHhDeGYymeBPlaOvnx-6LztSayqT9k5EGA2K4VHWDF4eafY8LSmLlFdVj9WVqelJKxmEKVe1uB5bshSiQRyYuE2vRDtyDlqA4T3SZJZC8r293AHZOLD54JCIGq2J61lqq1lVg=w960-h635-no',
+        cols: 6,
+    },{
+        url: 'https://lh3.googleusercontent.com/VkQwDtAei-d5HuABdYf_tLNkwJijSrRtnDfzcCUJwGN0jlMPTrPFCHr2f2_T-U37vNHk8pwVbHWVCFhmEaXqNJXhBylhwk7VWrWsGkX4vl3vY0JYlT6jElRsFDH8rVQ0VGmGiwXPGxmdpKRs_VEnvMecGxIAgGnoVsLgE1HP260VEpgX_8K1fd54EpaZ2wW05YP3FLvp-kGwQ5yjgUNxkrA40ZMDPaPMkY332gRg6YyiK_jAihNj-ZP1kFpe14VmPenQ2zcGLDs5vjh-UFGcm39b9RxgtfiqYriI4h6faHfoBZgoJjaS95iUqCgx84fRaSNSP_piegm6RluFXUYail2NRW3b30AvCt__tb0x3jAVWiFm3jFw2V0vDN0cQOyUsZOLsvzAPFvHwbY8_aOwfwddY5NfXwVS5--PsGKqjJWF8_0ktuiPk2NvI20ZvQfUaGiRTeZqbvzYxiAqJhH_FpIvIc93bur6YMAjP_QUnBPBzXXuIKw8KhOmYTdL3HOD-aeTew-jLDcI56QrMDEAXcS5sxWHLqKfRUFNXBDhY4KocnDR5afnWkqC1LKnokliMCs_7eeDz2Lyb0STzHifUI2oqc9JQCm5vXKtGJVsxQ=w1110-h720-no',
+        cols: 6
+    }]],
+    repertoire: [{
+        category: 'Opera',
+        items: [
+            { author: 'W.A. Mozart', line_1: '"Nozze di Figaro", role Cherubino, role Marcellina' },
+            { author: 'G. Verdi', line_1: '"La Traviata", role Flora' },
+            { author: 'N.Rimsly-Korsakov', line_1: '"The Tsar\'s Bride", role Liubasha' },
+            { author: 'G. Bizet', line_1: '"Carmen", role Carmen' },
+            { author: 'P.I.Tchaikovsky', line_1: '"Eugen Oneghin", role Olga' },
+            { author: 'U.Giordano', line_1: '"Andrea Chenier", role Bersi' },
+            { author: 'P.I.Tchaikovsky', line_1: '"Jolanta", role Laura' },
+            { author: 'G. Verdi', line_1: '"Rigoletto", role Maddalena, role Giovanna' },
+            { author: 'J. Massenet', line_1: '"Werther", role Charlotte' },
+            { author: 'G. Verdi', line_1: '"Nabucco", role Fenena' },
+            { author: 'J. Haydn', line_1: '"Il mondo della Luna", role Ernesto' }
+    ]},{
+        category: 'Oratories and Cantatas',
+        items: [
+            { author: 'G. Verdi', line_1: 'Reqiuem' },
+            { author: 'W.A. Mozart', line_1: 'Reqiuem'},
+            { author: 'A. Bruckner', line_1: 'Te Deum'}
+        ]
+    },{
+        category: 'Selected arias',
+        items: [
+            {author: 'P. Mascagni', line_1: 'Romanza di Santuzza, from opera Cavalleria Rusticana' },
+            {author: 'G.Verdi', line_1: 'aria Eboli "O don fatale" from opera Don Carlo'},
+            {author: 'G.Donizetti', line_1: 'recitativo ed aria di Leonora "Fia dunque e vero" , from opera "La Favorita"'},
+            {author: 'W.A. Mozart', line_1: 'recitativo ed aria di Dorabella "Ah, scostati", from opera "Cosi fantutte"'},
+            {author: 'R. Strauss', line_1: 'Komponist aria "Sein wir wieder gut" from opera "Ariadne auf Naxos"'},
+            {author: 'N.Rimsky-Korsakov', line_1: 'aria Liubavy from opera "Sadko"'},
+            {author: 'C. Saint-Saens', line_1: 'arias Dadila "Mon coer souve a ta voix" and "Samson recherchant ma presence" from opera "Samson et Dalila"'},
+            {author: 'M .Mussorgsky', line_1: 'Marfa\'s aria from "Khovanshchina"'},
+            {author: 'H.Purcell', line_1: 'recitativo and air of Dido from opera "Dido and Aenas"'},
+            {author: 'G.F. Handel', line_1: 'Alcina\'s aria from opera "Alcina"'},
+            {author: 'G.Rossini', line_1: 'aria di Berta from opera "Il barbiere di Siviglia"'},
+            {author: 'G .Donizetti', line_1: 'Smeton \'s aria "Deh non voler constringere" from opera "Anna Bolena'},
+            {author: 'K.Molchaninov', line_1: 'Zhenky\'s aria from opera "The dawns here are quiet"'},
+            {author: 'A.Dvorak', line_1: 'Jezibaba\'s aria "Cury mury fuk" from opera Rusalka'},
+            {author: 'C.W.Gluck', line_1: 'Orfeu\'s aria "J\'ai perdu mon Eurydice" from opera "Orphee et Eurydice"'}
+        ]
+    },{
+        category: 'Selected arias from Cantatas, Oratories and other musical cycles',
+        items: [
+            {author: 'A.Vivaldi', line_1: 'di Abra "Si fulgida per te" from oratorio "Juditha Triumphans"'},
+            {author: 'G.F.Hendel', line_1: '"Dignare"'},
+            {author: 'J.S.Bach', line_1: '"Erbarme dich" from oratorio "Matthaus Passion"'},
+            {author: 'J.S.Bach', line_1: '"Et exultavit" from cantata "Magnificat"'},
+            {author: 'G.B.Pergolesi', line_1: '"Eja Mater" from "Stabat Mater"'},
+            {author: 'A.Dvorak', line_1: '"Inflamatus" from cantata "Stabat Mater"'},
+            {author: 'A.Dvorak', line_1: '"Ave Maria"'},
+            {author: 'F.P. Shubert', line_1: '"Ave Maria"'},
+            {author: 'G. Caccini', line_1: '"Ave Maria"'},
+            {author: 'G. Caccini', line_1: '"Madrigal"'},
+            {author: 'L. Luzzi', line_1: '"Ave Maria"'},
+            {author: 'G. Faure', line_1: '"Ave Maria"'},
+            {author: 'Ch. Gounod', line_1: '"Ave Maria"'},
+            {author: 'P. Mascagni', line_1: '"Ave Maria" intermezzo from Cavalleria Rusticana'}
+        ]
+    },{
+        category: 'Duets',
+        items: [
+            {author: 'G. Franck', line_1: '"Panis Angelicus" "Ave Maria"'},
+            {author: 'A. Dvorak', line_1: '"O Sanctissima"'},
+            {author: 'G.B. Pergolesi', line_1: '"Stabat Mater"'},
+            {author: 'G.A. Rossini', line_1: '"Voga o Tonio" from "La regata Veneziana"'},
+            {author: 'A. Dvorak', line_1: 'Moravian Duets'},
+            {author: 'G. Verdi', line_1: 'duet Azucena ed Manrico "Si, la stanchezza" from opera "Il Trovatore"'},
+            {author: 'P. Mascagni', line_1: 'duet Santuzza ed Turiddu from opera "Cavalleria Rusticana"'},
+            {author: 'L. Delibes', line_1: 'Flower duet from opera "Lakme"'},
+            {author: 'J. Offenbach', line_1: 'duet Barcarolle from "The Tales of Hoffman"ka'},
+            {author: 'G. Verdi', line_1: 'duet Aida and Amneris from opera "Aida"'}
+        ]
+    },{
+        category: 'Romances and songs',
+        items: [
+            {author: 'S. Rachmaninov', line_1: '"Melody", "Ja zhdu tebja"'},
+            {author: 'A. Dargomyzhsky', line_1: '"Ja vseo ishio jevo ljubljiu", "Adelasi tumanom Grenada"'},
+            {author: 'M. Glinka', line_1: '"Skazhy zachem"'},
+            {author: 'A. Rubinstein', line_1: '"Noch"'},
+            {author: 'A. Dvorak', line_1: '"Kdyz mne stara matka"'},
+            {author: 'I.A. Lara', line_1: '"Granada"'},
+            {author: 'G. Rossini', line_1: '"Canzinetta Spagnuola"'},
+            {author: 'L. Rellstab', line_1: '"Standchen"'},
+            {author: 'F. Grieg', line_1: '"Mit Einer Wasserlilie"'},
+            {author: 'F. Shubert', line_1: '"An die Musik"'},
+            {author: 'J. Brahms', line_1: '"Liebestreu"'}
+        ]
+    }]
+  };
